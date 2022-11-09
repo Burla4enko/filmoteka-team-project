ae3a72c899314ced51693a45ca63fa878ea7608d
Burla4enko <45729176+Burla4enko@users.noreply.github.com>
2022-11-09 05:08:59 +0000

    Deploying to gh-pages from @ Burla4enko/filmoteka-team-project@b45c085ec9eb6f46eefaec764d3c58f2f6514578 🚀

---
+++ index.cdcdea21.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-team-modal-close]"),t=document.querySelector(".open-team-modal"),d=document.querySelector("div[data-modal-team]");function a(e){e.preventDefault(),d.classList.toggle("is-hidden")}e.addEventListener("click",a),t.addEventListener("click",a);
+//# sourceMappingURL=index.cdcdea21.js.map
